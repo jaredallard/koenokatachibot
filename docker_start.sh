e947a80e6c4796c344001412a7d01a6c10431815
Jared Allard <jaredallard@outlook.com>
2022-11-29 19:10:03 -0800

    fix docker script

--- docker_start.sh
+++ docker_start.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
 docker build -t knkb .
-docker run -it -v "$(pwd)/config:/srv/app/config" -v "$(pwd)/data:/srv/app/data" -d knkb
+docker run -it \
+  -v "$(pwd)/config:/srv/app/config:ro" \
+  -v "$(pwd)/data:/srv/app/data:ro" \
+  -v "$(pwd)/timestamps:/srv/app/timestamps" \
+  -d knkb
